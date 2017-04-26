18a1576ff991b2e6020ff4ccd8eadee32c819fc7
Raphael Tataia <raphaeltataia@Raphaels-MacBook-Pro.local>
2017-04-26 02:11:40 -0300

    bug fixes

--- ads/urls.py
+++ ads/urls.py
@@ -20,5 +20,5 @@ from bakery import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 ]
--- bakery/bakery/admin.py
+++ bakery/bakery/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Recipe
-from .models import Chief
+# from .models import Recipe
+# from .models import Chief
+from models import *
 
 # developed by @raphaeltataia
 
